4d816c123611d0b419e0038d7a09bb94c042eccf
Christoff Bothma <59228075+ChristoffBo@users.noreply.github.com>
2022-10-27 12:34:16 +0200

    Update run.sh

--- mimic3-tts-server/run.sh
+++ mimic3-tts-server/run.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
+
+echo ""
+echo "===================================================================================="
+echo ""Checking Mimic Server Install                                                     ""
+echo "===================================================================================="
+
+FILE="/tmp/mycroft-mimic3-tts_0.2.3_amd64.deb/"
+if [ -d "$FILE" ]; then
+
+ echo "Mimic is Already Installed"
+else
 cd /tmp
 apt install ./mycroft-mimic3-tts_0.2.3_amd64.deb
+ echo "Mimic not installed.Installing."
+fi
+
+echo ""
+echo "======================================================================================"
+echo ""Starting Mimic TTS Server                                                      ""
+echo "======================================================================================"
 
 cd /usr/bin
 
 mimic3-server
+
+exit 1
