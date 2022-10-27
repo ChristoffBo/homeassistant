6f4e1104d4eb8bd321710fd608b583784204ec3e
Christoff Bothma <59228075+ChristoffBo@users.noreply.github.com>
2022-10-27 13:36:11 +0200

    Update run.sh

--- mimic3-tts-server/run.sh
+++ mimic3-tts-server/run.sh
@@ -10,14 +10,15 @@ if [ -f "$FILE" ]; then
 
  echo "Mimic is Already Installed"
 else
-curl -J -L -o /tmp/mycroft-mimic3-tts_0.2.3_amd64.deb \
+mkdir -p /data/cache/
+curl -J -L -o /data/cache/mycroft-mimic3-tts_0.2.3_amd64.deb \
         "https://github.com/MycroftAI/mimic3/releases/download/release%2Fv0.2.3/mycroft-mimic3-tts_0.2.3_amd64.deb"
 mkdir -p /data/cache/
 chmod 777 /data/cache/
 chmod 777 /var/lib/apt/lists/auxfiles
 chmod 777 /var/cache/apt
 cd /tmp
-dpkg -i /tmp/mycroft-mimic3-tts_0.2.3_amd64.deb
+dpkg -i /data/cache/mycroft-mimic3-tts_0.2.3_amd64.deb
 apt-get install -f
  echo "Mimic not installed.Installing."
 fi
