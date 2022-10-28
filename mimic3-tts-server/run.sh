3a2ceb53db5281a24b3d925626e3e0eb35a63647
Christoff Bothma <59228075+ChristoffBo@users.noreply.github.com>
2022-10-28 10:57:42 +0200

    Update run.sh

--- mimic3-tts-server/run.sh
+++ mimic3-tts-server/run.sh
@@ -7,13 +7,15 @@ echo "==========================================================================
 echo ""Starting Mimic TTS Server                                                      ""
 echo "======================================================================================"
 
-
-mimic3-server --preload-voice en_US/ljspeech_low --cache-dir /data/cache/
-
 echo ""
 echo "======================================================================================"
 echo ""Checking Voices And Uncompressing                                                   ""
 echo "======================================================================================"
+
+
+mimic3-server --preload-voice en_US/ljspeech_low --cache-dir /data/cache/
+
+
 
 
 exit 1
