d8076e02afe75754f95c6b9400cd0094de5372fe
Christoff Bothma <59228075+ChristoffBo@users.noreply.github.com>
2022-11-22 11:00:52 +0200

    Add files via upload

---
+++ mimic3-tts-server/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+mkdir -p /data/mimic3
+/home/mimic3/app/ /root/.local/share/mycroft/mimic3/voices/ /usr/share/mycroft/mimic3/voices/
+
+
+echo ""
+echo "======================================================================================"
+echo ""Starting Mimic TTS Server                                                      ""
+echo "======================================================================================"
+
+
+
+
+
+exit 1
