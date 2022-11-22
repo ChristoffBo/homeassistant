c63bb42a2dce065d07cd7aa5e412c0860a93cdb9
Christoff Bothma <59228075+ChristoffBo@users.noreply.github.com>
2022-11-22 11:07:44 +0200

    Update run.sh

--- mimic3-tts-server/run.sh
+++ mimic3-tts-server/run.sh
@@ -5,12 +5,6 @@ mkdir -p /data/mimic3
 /home/mimic3/app/ /root/.local/share/mycroft/mimic3/voices/ /usr/share/mycroft/mimic3/voices/
 
 
-echo ""
-echo "======================================================================================"
-echo ""Starting Mimic TTS Server                                                      ""
-echo "======================================================================================"
-
-
 
 
 
