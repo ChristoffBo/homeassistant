c444772a0eb0fcd9e885854a78c167d47c92c9db
Christoff Bothma <59228075+ChristoffBo@users.noreply.github.com>
2022-11-22 11:07:18 +0200

    Update install.sh

--- technitium-dns/install.sh
+++ technitium-dns/install.sh
@@ -3,10 +3,6 @@
 mkdir -p /data/technitium
 /usr/bin/dotnet /etc/dns/DnsServerApp.dll /data/technitium
 
-echo ""
-echo "======================================================================================"
-echo ""Starting Technitium DNS Server                                                      ""
-echo "======================================================================================"
 
 exit 1
 
