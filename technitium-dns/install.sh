a594542c97195b446d9beba0a506fb723155fae8
Christoff Bothma <59228075+ChristoffBo@users.noreply.github.com>
2023-02-25 16:10:37 +0200

    Update install.sh

--- technitium-dns/install.sh
+++ technitium-dns/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p /data/technitium
-/usr/bin/dotnet /etc/dns/DnsServerApp.dll /data/technitium
+/usr/bin/dotnet /etc/dns/DnsServerApp.dll /data/technitium /opt/technitium/dns
 
 
 exit 1
