5974d2cabb24f98179754c46b50306da8b766791
Christoff Bothma <59228075+ChristoffBo@users.noreply.github.com>
2023-02-25 16:28:49 +0200

    Update run.sh

--- technitium-dns/run.sh
+++ technitium-dns/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p /data/technitium
-/usr/bin/dotnet /etc/dns/DnsServerApp.dll /data/technitium /opt/technitium/dns
+/usr/bin/dotnet /etc/dns/DnsServerApp.dll /data/technitium
 
 
 exit 1
