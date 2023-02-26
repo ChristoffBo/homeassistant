0bc9c9e4f3fc7ebafeeec8727556913801518052
Christoff Bothma <59228075+ChristoffBo@users.noreply.github.com>
2023-02-26 15:19:04 +0200

    Update run.sh

--- technitium-dns/run.sh
+++ technitium-dns/run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel STS
+
 mkdir -p /data/technitium
 /usr/bin/dotnet /opt/technitium/dns/DnsServerApp.dll /data/technitium
 
