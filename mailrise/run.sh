90e608404dce52b49c821c62a6640552cb15e232
Christoff Bothma <59228075+ChristoffBo@users.noreply.github.com>
2025-07-26 13:48:13 +0200

    Update run.sh

--- mailrise/run.sh
+++ mailrise/run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 CONFIG_PATH="/data/mailrise.conf"
-
-# Write config from Home Assistant options
 echo "$CONFIG" > "$CONFIG_PATH"
-
-# Run Mailrise using custom config
 exec mailrise --config "$CONFIG_PATH"
