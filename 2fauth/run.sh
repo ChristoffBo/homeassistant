0f3af253d0e2f506aabea7ed5c6ba782cb4d09b0
Christoff Bothma <59228075+ChristoffBo@users.noreply.github.com>
2025-07-26 20:23:02 +0200

    Update run.sh

--- 2fauth/run.sh
+++ 2fauth/run.sh
@@ -1,11 +1,21 @@
-ARG BUILD_FROM=2fauth/2fauth:latest
-FROM ${BUILD_FROM}
+#!/usr/bin/env bash
+set -e
+echo "[INFO] Starting 2FAuth..."
 
-# Install dependencies
-RUN apk add --no-cache bash docker-cli curl
-
-# Copy your run script
-COPY run.sh /run.sh
-RUN chmod +x /run.sh
-
-CMD ["/run.sh"]
+docker run -d \
+  --name=2fauth \
+  -e APP_URL="${APP_URL}" \
+  -e MAIL_ENCRYPTION="${MAIL_ENCRYPTION}" \
+  -e MAIL_FROM_ADDRESS="${MAIL_FROM_ADDRESS}" \
+  -e MAIL_FROM_NAME="${MAIL_FROM_NAME}" \
+  -e MAIL_HOST="${MAIL_HOST}" \
+  -e MAIL_MAILER="${MAIL_MAILER}" \
+  -e MAIL_PORT="${MAIL_PORT}" \
+  -e PGID="${PGID}" \
+  -e PUID="${PUID}" \
+  -e TZ="${TZ}" \
+  -p 8001:8000 \
+  -v /data:/2fauth \
+  --privileged \
+  --restart unless-stopped \
+  2fauth/2fauth:latest
