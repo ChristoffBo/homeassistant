46d80678ee731ad59fdca5ae9e401ed7e75e28d6
Christoff Bothma <59228075+ChristoffBo@users.noreply.github.com>
2025-07-26 13:50:35 +0200

    Update run.sh

--- mailrise/run.sh
+++ mailrise/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 CONFIG_PATH="/data/mailrise.conf"
+
+# Save config from GUI to file
 echo "$CONFIG" > "$CONFIG_PATH"
+
+# Run Mailrise with provided config
 exec mailrise --config "$CONFIG_PATH"
