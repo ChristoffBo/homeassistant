bfcf5a8ac4b3a7bb261ee3c45dd63cc4469d811c
Christoff Bothma <59228075+ChristoffBo@users.noreply.github.com>
2025-07-29 20:46:41 +0200

    Update run.sh

--- gitea/rootfs/run.sh
+++ gitea/rootfs/run.sh
@@ -1,7 +1,21 @@
-#!/bin/sh
-set -e
+#!/usr/bin/with-contenv bashio
 
+# Create directories
 mkdir -p /data/gitea/{conf,data,logs,repositories}
-[ ! -f /data/gitea/conf/app.ini ] && cp /etc/gitea/app.ini /data/gitea/conf/
+
+# Generate config if missing
+if [ ! -f /data/gitea/conf/app.ini ]; then
+    cp /etc/gitea/app.ini /data/gitea/conf/app.ini
+fi
+
+# Configure SSH
+crudini --set /data/gitea/conf/app.ini "server" \
+    DISABLE_SSH "$(! bashio::config 'ssh_enabled'; echo $?)"
+
+# Set permissions
 chown -R git:git /data/gitea
-exec su-exec git /usr/local/bin/gitea web --config /data/gitea/conf/app.ini
+chmod -R 750 /data/gitea
+
+# Start Gitea
+exec s6-setuidgid git \
+    /usr/local/bin/gitea web --config /data/gitea/conf/app.ini
