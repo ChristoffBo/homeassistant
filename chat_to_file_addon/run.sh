694ba9c5008e7d4790e9c2c828fde808521befcb
Christoff Bothma <59228075+ChristoffBo@users.noreply.github.com>
2025-08-01 10:24:09 +0200

    Update run.sh

--- chat_to_file_addon/run.sh
+++ chat_to_file_addon/run.sh
@@ -3,7 +3,7 @@
 # Set default output directory
 OUTPUT_DIR="/share/chat_to_file"
 
-# Create directory if it doesn't exist
+# Create directory with full permissions
 mkdir -p "$OUTPUT_DIR"
 chmod 777 "$OUTPUT_DIR"
 
