73bb4f23c7deab8ed4fb812b2acabdaef36b4e72
Christoff Bothma <59228075+ChristoffBo@users.noreply.github.com>
2025-08-01 12:47:59 +0200

    Update run.sh

--- ai_assistant/run.sh
+++ ai_assistant/run.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/with-contenv sh
 
-# Activate virtual environment
-. /venv/bin/activate
-
-# Start application as PID 1
-exec gunicorn \
-    --bind 0.0.0.0:5000 \
-    --worker-class sync \
-    --threads 1 \
-    --timeout 120 \
-    app.main:app
+# Start the application directly as PID 1
+exec /usr/bin/python3 /app/main.py
