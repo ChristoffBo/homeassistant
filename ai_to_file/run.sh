5ca4ee1af5c3deac5bb8251a0e85f4529a7a6e05
Christoff Bothma <59228075+ChristoffBo@users.noreply.github.com>
2025-08-01 11:35:51 +0200

    Update run.sh

--- ai_to_file/run.sh
+++ ai_to_file/run.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/with-contenv bashio
 
+# Activate virtual environment
+source /venv/bin/activate
+
 # Change to app directory
 cd /app || exit
 
-# Execute main.py directly (not as module)
-exec python3 /app/main.py
+# Execute main.py
+exec python3 main.py
