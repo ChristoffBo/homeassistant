45eb98bf5119cf392aa6bff08e56c405aaed1497
Christoff Bothma <59228075+ChristoffBo@users.noreply.github.com>
2025-08-01 17:47:17 +0200

    Create run.sh

---
+++ Advanced Addon Updater/run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/with-contenv bashio
+set -e
+
+# Setup SSH keys if they exist
+if [ -d "/config/.ssh" ]; then
+    mkdir -p /root/.ssh
+    cp -R /config/.ssh/* /root/.ssh/
+    chmod 600 /root/.ssh/*
+    chmod 700 /root/.ssh
+fi
+
+# Export Supervisor token for notifications
+export SUPERVISOR_TOKEN=$(bashio::supervisor.token)
+
+# Run Python updater
+python3 /updater.py
+
+bashio::log.info "Addon update process completed"
