8684e99a55429d85eb30eaacea513349808d45fe
Christoff Bothma <59228075+ChristoffBo@users.noreply.github.com>
2025-08-01 12:21:02 +0200

    Update run.sh

--- ai_assistant/run.sh
+++ ai_assistant/run.sh
@@ -1,1 +1,11 @@
+#!/usr/bin/with-contenv bashio
 
+# Load configuration
+export DEFAULT_MODEL=$(bashio::config 'default_model')
+export CHATGPT_KEY=$(bashio::config 'chatgpt_api_key')
+export DEEPSEEK_KEY=$(bashio::config 'deepseek_api_key')
+export GITHUB_TOKEN=$(bashio::config 'github_token')
+export GITHUB_REPO=$(bashio::config 'github_repo')
+
+cd /app
+exec python3 -m app.main
