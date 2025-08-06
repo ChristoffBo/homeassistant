76c49d06b43493b2976f185f35ddd7a2fd35b963
Christoff Bothma <59228075+ChristoffBo@users.noreply.github.com>
2025-08-06 16:25:34 +0200

    Delete git_commander directory

--- git_commander/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-chmod +x /app/toolkit.py /app/uploader.py /app/backup.py
-python3 /app/uploader.py &
-python3 /app/toolkit.py &
-python3 /app/backup.py
--- git_commander/www/app.js
+++
@@ -1,1 +0,0 @@
-console.log('Git Commander loaded');
