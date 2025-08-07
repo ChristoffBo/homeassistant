91123ceb1db6db97e59608ebd9740b5a3967d20e
Christoff Bothma <59228075+ChristoffBo@users.noreply.github.com>
2025-08-07 20:47:26 +0200

    Delete apprise/apprise directory

--- apprise/apprise/rootfs/etc/cont-init.d/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/with-contenv bash
-set -e
-
-CONFIG_PATH="/data/options.json"
-PORT=$(jq -r '.port // 8000' "$CONFIG_PATH")
-CLI_ARGS=$(jq -r '.cli_args // ""' "$CONFIG_PATH")
-
-echo "[INFO] Starting official Apprise web UI on port $PORT..."
-exec apprise-api --bind 0.0.0.0 --port "$PORT" $CLI_ARGS
