45b85433916f25c52ca8626aabf9fd24a1f2282c
Christoff Bothma <59228075+ChristoffBo@users.noreply.github.com>
2025-08-20 21:03:27 +0200

    Update run.sh

--- gotify/run.sh
+++ gotify/run.sh
@@ -2,9 +2,7 @@
 set -e
 
 # Prepare persistent dirs inside /config
-mkdir -p /config/gotify
-mkdir -p /config/gotify/images
-mkdir -p /config/gotify/plugins
+mkdir -p /config/gotify /config/gotify/images /config/gotify/plugins
 
 # Force Gotify to use /config for all persistence
 export GOTIFY_DATABASE_DIALECT=sqlite3
@@ -13,5 +11,5 @@ export GOTIFY_UPLOADEDIMAGESDIR=/config/gotify/images
 export GOTIFY_PLUGINSDIR=/config/gotify/plugins
 export GOTIFY_SERVER_PORT=80
 
-# Start Gotify
-exec /app/gotify
+# Start Gotify (official image binary path)
+exec /app/gotify-app
