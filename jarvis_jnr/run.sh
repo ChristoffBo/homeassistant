27834e2e4e638be6bfa19896645d517cc96e138b
Christoff Bothma <59228075+ChristoffBo@users.noreply.github.com>
2025-08-23 10:19:00 +0200

    Update run.sh

--- jarvis_jnr/run.sh
+++ jarvis_jnr/run.sh
@@ -5,14 +5,12 @@ CONFIG_PATH=/data/options.json
 
 echo "[Jarvis Jnr] Starting bot..."
 
-# Export all options as environment variables
+# Export options as env vars
 export BOT_NAME=$(jq -r '.bot_name' $CONFIG_PATH)
 export BOT_ICON=$(jq -r '.bot_icon' $CONFIG_PATH)
-
 export GOTIFY_URL=$(jq -r '.gotify_url' $CONFIG_PATH)
 export GOTIFY_CLIENT_TOKEN=$(jq -r '.gotify_client_token' $CONFIG_PATH)
 export GOTIFY_APP_TOKEN=$(jq -r '.gotify_app_token' $CONFIG_PATH)
-
 export RETENTION_HOURS=$(jq -r '.retention_hours' $CONFIG_PATH)
 
 export BEAUTIFY_ENABLED=$(jq -r '.beautify_enabled' $CONFIG_PATH)
